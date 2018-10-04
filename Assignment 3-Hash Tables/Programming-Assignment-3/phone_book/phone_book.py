f285efa26e998150fbb8bb43d82321dac3c11b79
Rayan Dasoriya <dasoriyarayan@gmail.com>
2018-10-04 16:14:50 -0600

    Here are the Optimized Programs

---
+++ Assignment 3-Hash Tables/Programming-Assignment-3/phone_book/phone_book.py
@@ -0,0 +1,44 @@
+# python3
+
+
+class PhoneBook:
+ 
+    
+    def __init__(self):
+        self.book = [None] * 10000000
+    
+    def add(self, number, name):
+        self.book[number] = name
+    
+    def delete(self, number):
+        if self.book[number] is not None:
+            self.book[number] = None
+
+    def find(self, number):
+        if self.book[number] is None:
+            return "not found"
+        return self.book[number]
+
+
+def process_queries(queries):
+    """Helper function which reads queries from standard input,
+        runs phonebook manager and sends the results to standard output.
+        """
+    for query in queries:
+        q = query.split()
+        cmd = q[0]
+        number = int(q[1])
+        if cmd == "add":
+            phonebook.add(number, q[2])
+        elif cmd == "find":
+            print(phonebook.find(number))
+        elif cmd == "del":
+            phonebook.delete(number)
+
+
+if __name__ == "__main__":
+    phonebook = PhoneBook()
+    
+    n = int(input())
+    queries = [input() for i in range(n)]
+    process_queries(queries)
